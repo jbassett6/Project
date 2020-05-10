3e12f52082a11c2acf2eb734443516f620e6183d
Jason Bassett <jbassett6@bcst01.parkland.edu>
2020-05-10 15:33:04 -0500

    added database and names

---
+++ version20-20/welcome.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>CSC155 001DR Survey Thing</title>
+<?php
+
+require ('library/functions.php');
+
+// depending on the zone, call one of
+checkAccount("none");
+//checkAccount("user");
+//checkAccount("admin");
+ 
+?>
+</head>
+<body>
+This is the welcome page for our project.
+
+If you don't have an account, you can create one <a href='createAccount.php'> here </a>. <br/>
+
+Login Form goes here
+</body>
