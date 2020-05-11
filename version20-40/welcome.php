e3cf6dc8028b3b348efd4ed946d1861a55352a38
Jason Bassett <jbassett6@bcst01.parkland.edu>
2020-05-10 22:24:44 -0500

    added encryption, first/lastname, email post, integrated database

---
+++ version20-40/welcome.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>CSC155 001DR Survey Thing</title>
+<?php
+
+require ('library/functions.php');
+
+// depending on the zone, call one of
+checkAccount("none");
+//checkAccount("user");
+//checkAccount("admin");
+ 
+?>
+</head>
+<body>
+
+This is the welcome page for our project.
+
+If you don't have an account, you can create one <a href='createAccount.php'> here </a>. <br/>
+This is where you can see the account database over the web <a href='showAccounts.php'> here </a>. <br/>
+
+Login Form goes here
+</body>
