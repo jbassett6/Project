da5f877b470187a45060c942d9a089a210172c7e
Jason Bassett <jbassett6@bcst01.parkland.edu>
2020-05-12 20:16:40 -0500

    finished through version20-62 objectives all condensed on version 20-60 folder

---
+++ version20-62/createAccount.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+<title>CSC155 001DR Survey Thing</title>
+<link rel="stylesheet" type="text/css" href="library/styles.css">
+<?php
+
+require ('library/functions.php');
+
+// depending on the zone, call one of
+checkAccount("none");
+//checkAccount("user");
+//checkAccount("admin");
+
+// get connection object
+$conn = getDBConnection();
+
+if (isset($_POST['selection'])) // form loaded itself
+{
+    if ($_POST['selection'] == "Create Account") // insert new record chosen
+    {
+	if ($_POST['password'] == $_POST['confirm'])
+        {
+	    // build SQL command SECURELY
+            // prepare
+	    $stmt = $conn->prepare("INSERT INTO users 
+                       (username, encrypted_password, usergroup, email) 
+                       VALUES (?, ?, ?, ?)" );
+	    // bind variable names and types
+	    $stmt->bind_param("ssss", $username, $encrypted_password, 
+                                  $usergroup, $email);
+
+	    $username=$_POST['username'];
+	    $encrypted_password=password_hash($_POST['password'], 
+                                          PASSWORD_DEFAULT);
+	    $usergroup='user';
+	    $email=$_POST['email'];
+
+	    // put the statement together and send it to the database
+	    $stmt->execute();
+            header("Location: welcome.php");
+	}
+	else 
+	{
+	    displayError("Passwords don't match");
+        }
+	    
+    }
+    if ($_POST['selection'] == "Cancel") // insert new record chosen
+    {
+        header("Location: welcome.php");
+    }
+}
+?>
+
+</head>
+<body>
+
+<form method='POST'>
+<div style='border-width: 2px'>
+<table id='userform'> 
+<tr>
+  <td>Username</td>
+  <td><input type='text' name='username' /></td>
+</tr>
+<tr>
+  <td>Password</td>
+  <td><input type='password' name='password' /> </td>
+</tr>
+<tr>
+  <td>Confirm Password</td>
+  <td> <input type='password' name='confirm' /></td>
+</tr>
+<tr>
+  <td>Email</td>
+  <td> <input type='text' name='email' /> </td>
+</tr>
+<tr>
+  <td colspan='2' style='text-align: center; background-color: white;'> 
+    <input type='submit' name='selection' value='Create Account' />
+    &nbsp;
+    <input type='submit' name='selection' value='Cancel' />
+  </td>
+</tr>
+<tr>
+  <td colspan='2' style='text-align: center; background-color: lightred;'>
+    Warning: This is class project and is not secure!  
+  </td>
+</tr>
+</table>
+</div>
+</form>
+
+</body>
+</html>
